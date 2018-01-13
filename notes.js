9a99ac67d13ecc6c84b9f7613990a123db03718e
Bart <bart@paybase.io>
2018-01-13 21:38:43 +0000

    reading the notes from json file to persist notes

--- notes.js
+++ notes.js
@@ -3,11 +3,15 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 const addNote = (title, body) => {
-  const notes = [];
+  let notes = [];
   const note = {
     title,
     body,
   };
+
+  const notesString = fs.readFileSync('notes-data.json');
+  notes = JSON.parse(notesString);
+
   fs.writeFileSync('notes-data.json', JSON.stringify(notes.concat(note)), (err) => {
     if (err) throw err;
   });
