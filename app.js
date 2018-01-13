08ecc550ee7aa274d5e88895c4b879560d79e221
Bart <bart@paybase.io>
2018-01-13 21:33:36 +0000

    writing jsonified notes to a file

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 console.log('Starting app...');
 
 const fs = require('fs');
-const _ = require('lodash');
+// const _ = require('lodash');
 
 const notes = require('./notes.js');
 const yargs = require('yargs');
 
 const { argv } = yargs;
-const command = process.argv[2];
-console.log('command:', command);
-console.log('process', process.argv);
-console.log('YARGS', argv);
-
+const command = argv._[0];
 
 if (command === 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing notes');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Reading notes');
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing notes');
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,1 +1,32 @@
 console.log('Starting notes.js');
+
+const fs = require('fs');
+
+const addNote = (title, body) => {
+  const notes = [];
+  const note = {
+    title,
+    body,
+  };
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes.concat(note)), (err) => {
+    if (err) throw err;
+  });
+};
+
+const getAll = () => {
+  console.log('Getting notes');
+};
+
+const getNote = (title) => {
+  console.log('Reading note: ', title);
+};
+
+const removeNote = (title) => {
+  console.log('Deleting note: ', title);
+};
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+};
